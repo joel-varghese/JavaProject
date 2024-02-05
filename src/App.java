ed000ab337dec550d698619aba9ebeff37192efd
joel-varghese <joelvar17@outlook.com>
2024-02-04 21:22:18 -0500

    Added remaining opcodes and included location part

--- src/App.java
+++ src/App.java
@@ -15,7 +15,25 @@ public class App {
         map.put("LDX","04");
         map.put("LDR", "01");
         map.put("LDA", "03");
+        map.put("STX", "05");
         map.put("JZ", "06");
+        map.put("JNE", "07");
+        map.put("JCC", "10");
+        map.put("JMA", "11");
+        map.put("JSR", "12");
+        map.put("RFS", "13");
+        map.put("SOB", "14");
+        map.put("JGE", "15");
+        map.put("AMR", "16");
+        map.put("SMR", "17");
+        map.put("AIR", "20");
+        map.put("SIR", "21");
+        map.put("MLT", "22");
+        map.put("DVD", "23");
+        map.put("TRR", "24");
+        map.put("AND", "25");
+        map.put("ORR", "26");
+        map.put("NOT", "27");
         map.put("SETCCE", "44");
         map.put("Data","0");
         map.put("LOC","0");
@@ -39,13 +57,29 @@ public class App {
         "          LDR     1,2,10,1    ;R1 GETS 18",
         "          LDA     0,0,0       ;R0 GETS 0 to set CONDITION CODE",
         "          LDX     1,8         ;X1 GETS 1024",
+        "          DVD     2,1         ;",
+        "          TRR     3,0         ;",
+        "          JSR     2,9,1       ;",
         "          SETCCE  0           ;SET CONDITION CODE FOR EQUAL",
         "          JZ      1,0         ;JUMP TO End if CC is 1",
         "          LOC     1024",
         "End:      HLT                 ;STOP"
         };
+        String trimmedInput = vals[0].trim().replaceAll("\\s+", " ").replaceAll(" *;.*", "");
+        // Regular expression pattern to extract opcode and numbers
+        Pattern pattern = Pattern.compile("([A-Za-z]+)\\s*(\\d+)(?:,(\\d+))?(?:,(\\d+))?(?:,(\\d+))?");
+        Matcher matcher = pattern.matcher(trimmedInput);
+        String index = "0";
+        if(matcher.find()){
+            index = matcher.group(2);
+        }
+        int decimalInd = Integer.parseInt(index);
+        String serial = "000000";
         for(int i=0;i<vals.length;i++){
-            extractOpcodeAndNumbers(vals[i]);
+            String opcode = extractOpcodeAndNumbers(vals[i]);
+            String ans = "";
+            if(i==0){ans = serial+"   "+opcode;}else{ans = integerToOctal(decimalInd, 6) +"   "+opcode;decimalInd++;}
+            System.out.println(ans);
         }
     }
 
@@ -61,6 +95,14 @@ public class App {
 
         // Convert integer to octal string
         return Integer.toOctalString(num);
+    }
+
+    public static String integerToOctal(int val,int digits) {
+        // Convert binary string to integer
+
+        // Convert integer to octal string
+        String octal = Integer.toOctalString(val);
+        return padBinary(octal, digits);
     }
 
     private static String padBinary(String binary, int targetLength) {
@@ -89,7 +131,7 @@ public class App {
         }
     }
     
-    private static void extractOpcodeAndNumbers(String input) {
+    private static String extractOpcodeAndNumbers(String input) {
 
             String trimmedInput = input.trim().replaceAll("\\s+", " ").replaceAll(" *;.*", "");
             // Regular expression pattern to extract opcode and numbers
@@ -108,7 +150,7 @@ public class App {
                 if(map.get(opcode) != null){binary1 = decimalToBinary(map.get(opcode));}
                 binary1 = padBinary(binary1, 6);
                 // System.out.print(binary1);
-                if((opcode.equals("LDR")) || (opcode.equals("LDA")) || (opcode.equals("STR"))){
+                if((opcode.equals("LDR")) || (opcode.equals("LDA")) || (opcode.equals("STR") || opcode.equals("JCC")|| opcode.equals("SOB")|| opcode.equals("JGE")|| opcode.equals("AMR")|| opcode.equals("SMR"))){
                     String reg = decimalToBinary(matcher.group(2));
                     binary2 = padBinary(reg, 2);
                     String ix = decimalToBinary(matcher.group(3));
@@ -121,7 +163,7 @@ public class App {
                         binary4 = "0";
                     }
                     result = binary1 + binary2 + binary3 + binary4 + binary5;
-                }else if(opcode .equals("LDX") || opcode.equals("STX") || opcode.equals("JZ")){
+                }else if(opcode .equals("LDX") || opcode.equals("STX") || opcode.equals("JZ") || opcode.equals("JNE")|| opcode.equals("JMA")|| opcode.equals("JSR")){
                     String reg = "";
                     binary2 = "00";
                     String ix = decimalToBinary(matcher.group(2));
@@ -143,11 +185,21 @@ public class App {
                     binary5 = padBinary(addr, 5);
                     binary4 = "0";
                     result = binary1 + binary2 + binary3 + binary4 + binary5;
-                }else if(opcode.equals("SETCCE")){
+                }else if(opcode.equals("SETCCE")|| opcode.equals("RFS")){
+
                     String reg = decimalToBinary(matcher.group(2));
                     binary2 = padBinary(reg, 2);
                     String ix = "";
                     binary3 = "00";
+                    String addr = "";
+                    binary5 = "00";
+                    binary4 = "0";
+                    result = binary1 + binary2 + binary3 + binary4 + binary5;
+                }else if(opcode.equals("MLT") || opcode.equals("DVD") || opcode.equals("TRR") || opcode.equals("AND") || opcode.equals("ORR")){
+                    String reg = decimalToBinary(matcher.group(2));
+                    binary2 = padBinary(reg, 2);
+                    String ix = decimalToBinary(matcher.group(3));
+                    binary3 = padBinary(ix, 2);
                     String addr = "";
                     binary5 = "00";
                     binary4 = "0";
@@ -171,7 +223,8 @@ public class App {
                     result = "000000";
                 }
             }
-            System.out.println(result + input);
+            String answer = result+input;
+            return answer;
     }
     
 
